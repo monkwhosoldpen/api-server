7589833b9c8f7a23add9abe9d24f6408e906941f
ashwath-bharadwaj <166688960+ashwath-bharadwaj@users.noreply.github.com>
2024-06-16 02:15:50 +0530

    Sample

--- src/pages/api/v1/creator-token/top/index.ts
+++ src/pages/api/v1/creator-token/top/index.ts
@@ -1,4 +1,6 @@
 import { allowedOrigins, supabase } from "@pages/api/api-utils";
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('https://grew-faster.pockethost.io');
 
 export default async function handler(req, res) {
     // Set CORS headers
@@ -19,6 +21,19 @@ export default async function handler(req, res) {
 
     if (req.method === 'GET') {
         const userProfiles = [];
+        const resultList = await pb.collection('goats').getList(1, 50, {
+            filter: 'created >= "2022-01-01 00:00:00" && someField1 != someField2',
+        });
+        
+        // you can also fetch all records at once via getFullList
+        const records = await pb.collection('goats').getFullList({
+            sort: '-created',
+        });
+        
+        // or fetch only the first record that matches the specified filter
+        const record = await pb.collection('goats').getFirstListItem('someField="test"', {
+            expand: 'relField1,relField2.subRelField',
+        });
         // const { data: userProfiles, error } = await supabase
         // .from('user_profiles') // Replace 'user_profiles' with your actual table name
         // .select('*')
@@ -32,7 +47,7 @@ export default async function handler(req, res) {
         //     return res.status(500).json({ error: 'An unexpected error occurred' });
         // }
         const topCreatorTokenResponse = {
-            creator_tokens: [...userProfiles,]
+            creator_tokens: [...records || [],]
         };
         res.status(200).json(topCreatorTokenResponse);
     }
