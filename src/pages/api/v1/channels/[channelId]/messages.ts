120a45c0aabe6b049040c80cb42198a4890565a0
monkwhosoldpen <monkwhosoldpen@gmail.com>
2024-11-14 13:48:48 +0530

    Update messages.ts

--- src/pages/api/v1/channels/[channelId]/messages.ts
+++ src/pages/api/v1/channels/[channelId]/messages.ts
@@ -24,39 +24,39 @@ export default async function handler(req, res) {
     const parsedPage = parseInt(Array.isArray(page) ? page[0] : page) || 1;
     const parsedLimit = parseInt(Array.isArray(limit) ? limit[0] : limit) || 20;
 
-    console.log(channelId);
+    // console.log(channelId);
 
-    // Fetch messages from Supabase
-    let { data: messages, error } = await supabaseAnon
-      .from('zulip_messages')
-      .select('*')
-      .eq('username', channelId)
-      .range((parsedPage - 1) * parsedLimit, parsedPage * parsedLimit - 1);
+    // // Fetch messages from Supabase
+    // let { data: messages, error } = await supabaseAnon
+    //   .from('messages')
+    //   .select('*')
+    //   .eq('username', 'elonmusk')
+    //   .range((parsedPage - 1) * parsedLimit, parsedPage * parsedLimit - 1);
 
-    if (error) {
-      return res.status(500).json({ error: 'Error fetching messages' });
-    }
+    // if (error) {
+    //   return res.status(500).json({ error: 'Error fetching messages' });
+    // }
     
-    // Format messages like the sample message at index 0
-    const formattedMessages = messages.map(message => {
-      return {
-        channel_message: {
-          id: message.message_id,
-          created_at: message.timestamp,
-          updated_at: message.timestamp,
-          is_payment_gated: false,
-          body: message.content.text,
-          body_text_length: 243,
-          sent_by: {
-            admin: true,
-            profile: [Object]
-          }
-        },
-        read: true
-      }
-    });
+    // // Format messages like the sample message at index 0
+    // const formattedMessages = messages.map(message => {
+    //   return {
+    //     channel_message: {
+    //       id: message.message_id,
+    //       created_at: message.timestamp,
+    //       updated_at: message.timestamp,
+    //       is_payment_gated: false,
+    //       body: message.content.text,
+    //       body_text_length: 243,
+    //       sent_by: {
+    //         admin: true,
+    //         profile: [Object]
+    //       }
+    //     },
+    //     read: true
+    //   }
+    // });
 
-    return res.status(200).json(formattedMessages);
+    return res.status(200).json(mockChannelMessageData);
   } else {
     return res.status(405).json({ error: 'Method not allowed' });
   }
